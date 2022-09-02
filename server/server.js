0b2bc6813597187e8faf71bfc40617fe0f5247ca
Sam Freeman <samcfreeman888@gmail.com>
2022-09-02 13:11:50 -0500

    client and server are linked as well as all public side files

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const PORT = 5000;
+
+
+// This must be added before GET & POST routes.
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Serve up static files (HTML, CSS, Client JS)
+app.use(express.static('server/public'));
+
+// GET & POST Routes go here
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log ('Knock Knock!', PORT)
+})
