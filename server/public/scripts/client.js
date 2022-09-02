0553f0143410920f368a3b1f0bd321497fe5ab45
Sam Freeman <samcfreeman888@gmail.com>
2022-09-02 16:23:50 -0500

    object is ready to send, disabling other buttons once operator is selected is ready

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,22 +1,77 @@
 $(document).ready(handleReady);
 
 function handleReady() {
- $('#submitButton').on('click', sendNumbersToServer)
- $('#clearButton').on('click', clearInputs)
+ $('#submitButton').on('click', sendNumbersToServer);
+ $('#clearButton').on('click', clearInputs);
+ $('#plusButton').on('click', plusSelected);
+ $('#minusButton').on('click', minusSelected);
+ $('#multiplyButton').on('click', multiplySelected);
+ $('#divideButton').on('click', divideSelected);
 }
+
+let operator;
 
 function sendNumbersToServer (){
     let mathObject = {
         input1: $('#num1').val(), //currently strings
+        operator: operator,
         input2: $('#num2').val(),
-        //how to do operator?
+        
     }
     console.log(mathObject);
     $('#num1').val('');
     $('#num2').val('');
+    resetCalculator(); 
+}
+
+function plusSelected (){
+    operator = '+';
+    $('#minusButton').prop('disabled', true);
+    $('#multiplyButton').prop('disabled', true);
+    $('#divideButton').prop('disabled', true);
+    console.log(operator);
+}
+
+
+function minusSelected (){
+    operator = '-';
+    $('#plusButton').prop('disabled', true);
+    $('#multiplyButton').prop('disabled', true);
+    $('#divideButton').prop('disabled', true);
+    console.log(operator);
+}
+
+function multiplySelected (){
+    operator = 'x';
+    $('#plusButton').prop('disabled', true);
+    $('#minusButton').prop('disabled', true);
+    $('#divideButton').prop('disabled', true);
+    console.log(operator);
+}
+
+function divideSelected (){
+    operator = '/';
+    $('#plusButton').prop('disabled', true);
+    $('#minusButton').prop('disabled', true);
+    $('#divideButton').prop('disabled', true);
+    console.log(operator);
+}
+
+
+function resetCalculator (){
+    $('#plusButton').prop('disabled', false);
+    $('#minusButton').prop('disabled', false);
+    $('#multiplyButton').prop('disabled', false);
+    $('#divideButton').prop('disabled', false);
 }
 
 function clearInputs (){
     $('#num1').val('');
     $('#num2').val('');
+    $('#plusButton').prop('disabled', false);
+    $('#minusButton').prop('disabled', false);
+    $('#multiplyButton').prop('disabled', false);
+    $('#divideButton').prop('disabled', false);
 }
+
+//do I need to reset operator between calculations?
